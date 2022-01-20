28a470344a5b94f116530579a36c8e4b352d4249
fsosn <95348487+fsosn@users.noreply.github.com>
2022-01-20 20:12:03 +0100

    utworzenie plik nagłówkowego z bazą

---
+++ baza.h
@@ -0,0 +1,9 @@
+#ifndef _BAZA_H_IS_INCLUDED_
+#define _BAZA_H_IS_INCLUDED_
+
+double fi(double a, double b, int n, int i, double x);
+double dfi(double a, double b, int n, int i, double x);
+double d2fi(double a, double b, int n, int i, double x);
+double d3fi(double a, double b, int n, int i, double x);
+
+#endif
